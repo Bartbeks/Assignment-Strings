b62fcc08f9ba14576b64358afc4ad7642aa2a842
t-tauri <bart@t-tauri.nl>
2022-06-08 10:21:50 +0200

    stringassignment

---
+++ Desktop/winc/strings/main.py
@@ -0,0 +1,34 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+player_van_basten= 'Marco van Basten'
+player_gullit= 'Ruud Gullit'
+goal_0=32
+goal_1=54
+
+scorers = player_gullit +" "+str(goal_0)+","+" "+player_van_basten+" "+str(goal_1) 
+
+# report = f'{player_gullit} scored in the {str(goal_0)}nd minute {player_van_basten} scored in the {str(goal_1)}th minute'
+report = f'{player_gullit} scored in the {goal_0}nd minute\n{player_van_basten} scored in the {goal_1}th minute'
+
+# Choose a player that played in the soccer match and store his name as a string in the variable player.
+player ="Jan Wouters"
+
+first_name = player[0:3]
+last_name =player[4:11]
+
+last_name_len = len(last_name)
+name_short= f'{first_name[0:1]}. {last_name}'
+x= len(first_name)
+exclamationmark="!"
+
+chant= f'{first_name}! '*x  
+chant=chant[0:-1]
+
+good_chant =chant!='';
+
+
+       
+
